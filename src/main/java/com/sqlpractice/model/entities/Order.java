cdc26021b765a2b8e79d5203f78ae86851cdbea8
Geovane <geovane.hau.franca.pires@gmail.com>
2024-06-30 10:55:44 -0300

    Implemented composit PK logic between Order and Product classes inside OrderItem class using OrderItemPK to serve as Composit Primary Key. Implemented other missing methods in Order class.

--- src/main/java/com/sqlpractice/model/entities/Order.java
+++ src/main/java/com/sqlpractice/model/entities/Order.java
@@ -1,6 +1,8 @@
 package com.sqlpractice.model.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.sqlpractice.model.entities.enums.OrderStatus;
 
@@ -10,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,6 +30,11 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
+
+
     
     public Order(Long id, Instant moment, Integer orderStatus) {
         this.id = id;
@@ -58,6 +66,35 @@ public class Order {
             this.orderStatus = orderStatus.getCode();
         }
         
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Order other = (Order) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
     
---
+++ src/main/java/com/sqlpractice/model/entities/OrderItem.java
@@ -0,0 +1,53 @@
+package com.sqlpractice.model.entities;
+
+import java.io.Serializable;
+
+import com.sqlpractice.model.entities.PK.OrderItemPK;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_ORDER_ITEM")
+public class OrderItem implements Serializable{
+
+    @Embedded
+    private OrderItemPK id = new OrderItemPK();
+
+    private Integer quantity;
+    private Double price;
+
+
+    public OrderItem(Integer quantity, Double price, Order order, Product product) {
+        this.quantity = quantity;
+        this.price = price;
+        this.id.setOrder(order);
+        this.id.setProduct(product);
+    }
+
+    public OrderItemPK getId() {
+        return id;
+    }
+    public void setId(OrderItemPK id) {
+        this.id = id;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double subTotal(){
+        return quantity * price;
+    }
+
+    
+}
